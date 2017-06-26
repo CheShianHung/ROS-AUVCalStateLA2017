5315929d335d769b89d6d6e480b36d4e8054b1d4
cheshianhung <hungc@bgsu.edu>
2017-06-26 17:45:24 -0400

    publishers and subscribers added to master node

--- auv_cal_state_la_2017/src/master.cpp
+++ auv_cal_state_la_2017/src/master.cpp
@@ -4,15 +4,31 @@
 #include "std_msgs/Float32.h"
 #include "auv_cal_state_la_2017/HControl.h"
 #include "auv_cal_state_la_2017/RControl.h"
+#include "auv_cal_state_la_2017/MControl.h"
+#include "auv_cal_state_la_2017/FrontCamDistance.h"
+#include "auv_cal_state_la_2017/BottomCamDistance.h"
+#include "auv_cal_state_la_2017/TargetInfo.h"
 #include <sstream>
 
 // height_control: (int) state, (float) depth
 // state: going down (0), staying (1), going up (2)
 // depth: nonstop moving (-1), moving distance (x)
 
-// rotation_control: (int)state, (float) rotation
+// rotation_control: (int) state, (float) rotation
 // state: rotate left (0), staying (1), rotate right (2)
 // rotation: nonstop rotating (-1), rotate degree (x)
+
+// movement_control: (int) state, (int) mDirection, (float) power, (float) distance
+// state: off (0), adjust with power (1), ajust with distance (2), centered with front camera (3), centered with bottom camera (4)
+// mDirection: none (0), forward (1), right (2), backward (3), left(4)
+// power: none (0), motor power (x)
+// distance: distance away from the object (x)
+
+// target_info: (int) state, (float) angle, (float) height, (int) direction
+// state: not found (0), found (1)
+// angle: angles to turn left (x < 0), none (0), angles to turn right (x > 0)
+// height: depth to go down (x < 0), none (0), depth to go up (x > 0)
+// direction: left (-1), none (0), right (1)
 
 // Task List
 // Task 0: sub is under water
@@ -23,12 +39,17 @@
 //ROS variables
 auv_cal_state_la_2017::HControl hControl;
 auv_cal_state_la_2017::RControl rControl;
+auv_cal_state_la_2017::MControl mControl;
 
 //Subscriber callback functions
 void currentDepthCallback(const std_msgs::Float32& currentDepth);
-void hControlStatusCallback(const auv_cal_state_la_2017::HControl);
 void currentRotationCallback(const std_msgs::Float32& currentRotation);
-void rControlStatusCallback(const auv_cal_state_la_2017::RControl);
+void hControlStatusCallback(const auv_cal_state_la_2017::HControl hc);
+void rControlStatusCallback(const auv_cal_state_la_2017::RControl rc);
+void mControlStatusCallback(const auv_cal_state_la_2017::MControl mc);
+void frontCamDistanceCallback(const auv_cal_state_la_2017::FrontCamDistance fcd);
+void bottomCamDistanceCallback(const auv_cal_state_la_2017::BottomCamDistance bcd);
+void targetInfoCallback(const auv_cal_state_la_2017::TargetInfo ti);
 
 //Regular functions
 void checkMotorNode();
@@ -42,6 +63,10 @@ bool checkingCurrentDepth;
 bool checkingHeightControl;
 bool checkingCurrentRotation;
 bool checkingRotationControl;
+bool checkingMovementControl;
+bool checkingFrontCamDistance;
+bool checkingBottomCamDistance;
+bool checkingTargetInfo;
 bool motorNodeIsReady;
 bool allNodesAreReady;
 
@@ -66,17 +91,26 @@ int main(int argc, char **argv){
   ros::Subscriber currentRotationSubscriber = node.subscribe("current_rotation", 100, currentRotationCallback);
   ros::Subscriber hControlStatusSubscriber = node.subscribe("height_control_status", 100, hControlStatusCallback);
   ros::Subscriber rControlStatusSubscriber = node.subscribe("rotation_control_status", 100, rControlStatusCallback);
+  ros::Subscriber mControlStatusSubscriber = node.subscribe("movement_control_status", 100, mControlStatusCallback);
+  ros::Subscriber frontCamDistanceSubscriber = node.subscribe("front_cam_distance", 100, frontCamDistanceCallback);
+  ros::Subscriber bottomCamDistanceSubscriber = node.subscribe("bottom_cam_distance", 100, bottomCamDistanceCallback);
+  ros::Subscriber targetInfoSubscriber = node.subscribe("taget_info", 100, targetInfoCallback);
   ros::Publisher hControlPublisher = node.advertise<auv_cal_state_la_2017::HControl>("height_control", 100);
   ros::Publisher rControlPublisher = node.advertise<auv_cal_state_la_2017::RControl>("rotation_control", 100);
+  ros::Publisher mControlPublisher = node.advertise<auv_cal_state_la_2017::MControl>("movement_control", 100);
   ros::Rate loop_rate(10);
-
+  
   //currentTargetDepth = 0;
   //currentTargetRotation = 0;
 
   checkingCurrentDepth = false;
   checkingHeightControl = false;
+  checkingFrontCamDistance = false;
+  checkingBottomCamDistance = false;
   checkingCurrentRotation = false;
   checkingRotationControl = false;
+  checkingMovementControl = false;
+  checkingTargetInfo = false;
   motorNodeIsReady = false;
   allNodesAreReady = false;
 
@@ -96,26 +130,29 @@ int main(int argc, char **argv){
 
   //Checking nodes...
   while(ros::ok() && !allNodesAreReady){
-     
-    //Checking height_control...
     if(!checkingHeightControl){
       hControl.state = 1;
       hControl.depth = 0;
       hControlPublisher.publish(hControl);
     }
-
     if(!checkingRotationControl){
       rControl.state = 1;
       rControl.rotation = 0;
       rControlPublisher.publish(rControl);
     }
-     
+    if(!checkingMovementControl){
+      mControl.state = 0;
+      mControl.mDirection = 0;
+      mControl.power = 0;
+      mControl.distance = 0;
+      mControlPublisher.publish(mControl);
+    }
+
     ros::spinOnce();
     
     if(motorNodeIsReady){
       allNodesAreReady = true;
     }
-    
     loop_rate.sleep();
   }
   
@@ -199,7 +236,7 @@ void currentDepthCallback(const std_msgs::Float32& currentDepth){
 }
 
 void currentRotationCallback(const std_msgs::Float32& currentRotation){
-  //Checking current rotation...
+  //Checking current_rotation...
   if(!checkingCurrentRotation){
     checkingCurrentRotation = true;
     ROS_INFO("current_rotation is ready.");
@@ -214,6 +251,41 @@ void currentRotationCallback(const std_msgs::Float32& currentRotation){
   //Task 3 - Emerge 5 ft
   else if(!task3Finished){}
 }
+
+void frontCamDistanceCallback(const auv_cal_state_la_2017::FrontCamDistance fcd){
+  //Checking front_cam_distance...
+  if(!checkingFrontCamDistance){
+    checkingFrontCamDistance = true;
+    ROS_INFO("front_cam_distance is ready.");
+    checkMotorNode();
+  }
+  //Task 0 - Submerge under water
+  else if(!task0Finished){}
+  //Task 1 - Submerging 8 ft
+  else if(!task1Finished){}
+  //Task 2 - Rotate right 90 degrees
+  else if(!task2Finished){}
+  //Task 3 - Emerge 5 ft
+  else if(!task3Finished){}
+}
+
+void bottomCamDistanceCallback(const auv_cal_state_la_2017::BottomCamDistance bcd){
+  //Checking bottom_cam_distance...
+  if(!checkingBottomCamDistance){
+    checkingBottomCamDistance = true;
+    ROS_INFO("bottom_cam_distance is ready.");
+    checkMotorNode();
+  }
+  //Task 0 - Submerge under water
+  else if(!task0Finished){}
+  //Task 1 - Submerging 8 ft
+  else if(!task1Finished){}
+  //Task 2 - Rotate right 90 degrees
+  else if(!task2Finished){}
+  //Task 3 - Emerge 5 ft
+  else if(!task3Finished){}
+}
+
 
 void hControlStatusCallback(const auv_cal_state_la_2017::HControl hc){
   //Checking height_control...
@@ -297,12 +369,58 @@ void rControlStatusCallback(const auv_cal_state_la_2017::RControl rc){
   else if(!task3Finished){}
 }
 
+void mControlStatusCallback(const auv_cal_state_la_2017::MControl mc){
+  //Checking movement_control...
+  if(!checkingMovementControl){
+    ROS_INFO("Checking rotation_control...");
+    if(mc.state == 0 && mc.mDirection == 0 && mc.power == 0 && mc.distance == 0){
+      checkingMovementControl = true;
+      ROS_INFO("movement_control is ready.");
+      checkMotorNode();
+    }
+    else{
+      ROS_INFO("Cannot communicate with movement_control...");
+    }
+  }
+  //Task 0 - Submerge under water
+  else if(!task0Finished){}
+  //Task 1 - Submerge 8 ft
+  else if(!task1Finished){}
+  //Task 2 - Rotate right 90 degrees
+  else if(!task2Finished){}
+  //Task 3 - Emerge 5 ft
+  else if(!task3Finished){}
+}
+
+void targetInfoCallback(const auv_cal_state_la_2017::TargetInfo ti){
+  //Checking target_info...
+  if(!checkingTargetInfo){
+    ROS_INFO("Checking target_info...");
+    if(ti.state == 0 && ti.angle == 0 && ti.height == 0 && ti.direction == 0){
+      checkingTargetInfo = true;
+      ROS_INFO("target_info is ready.");
+      checkMotorNode();
+    }
+  }
+  //Task 0 - Submerge under water
+  else if(!task0Finished){}
+  //Task 1 - Submerge 8 ft
+  else if(!task1Finished){}
+  //Task 2 - Rotate right 90 degrees
+  else if(!task2Finished){}
+  //Task 3 - Emerge 5 ft
+  else if(!task3Finished){}
+}
+
 void checkMotorNode(){
   if(checkingCurrentDepth && 
      checkingHeightControl && 
      checkingCurrentRotation &&
-     checkingRotationControl){
+     checkingRotationControl &&
+     checkingMovementControl &&
+     checkingFrontCamDistance &&
+     checkingBottomCamDistance){
     motorNodeIsReady = true;
-    ROS_INFO("Motor node is ready...");
+    ROS_INFO("Motor node is ready...\n");
   }
 }
