1bb991d558bb68526fdef4d27eca60da398fe941
cheshianhung <hungc@bgsu.edu>
2017-07-12 17:26:50 -0700

    tcpip two ways communication adder added

---
+++ MatlabCommunication/tcpipTwoWaysCom_adder.m
@@ -0,0 +1,19 @@
+client = tcpip('10.0.0.2',55000,'NetworkRole','Client');
+set(client,'InputBufferSize',7688);
+set(client,'Timeout',30);
+fopen(client);
+
+server = tcpip('0.0.0.0',55000,'NetworkRole','Server');
+s = whos('data');
+set(server,'OutputBufferSize',s.bytes);
+fopen(server);
+
+while 1
+    data = fread(client,1,'double');
+    data = data + 0.1;
+    fwrite(server,data(:),'double');
+    disp(data);
+    pause(1);
+end
+
+fclose(client);
